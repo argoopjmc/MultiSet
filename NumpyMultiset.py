e15e4eea68a481b44df57fb84b7576688e36bd12
Rahul Gupta <46709364+argoopjmc@users.noreply.github.com>
2021-12-08 12:38:11 +0000

    Add skeleton for other functions

--- NumpyMultiset.py
+++ NumpyMultiset.py
@@ -44,3 +44,66 @@ class NumpyMultiset():
 
     def __str__(self):
         return f"{self.multiset}"
+
+    def supportSet(self):
+        return self.multiset['supports']
+
+    def multiplicities(self):
+        return self.multiset['multiplicities']
+
+    def typecheck(self, Multiset):
+        return type(Multiset.supportSet()[0])
+
+    def __eq__(self, other):
+        pass
+
+    def __ne__(self, other):
+        pass
+
+    def __le__(self, other):
+        pass
+
+    def __lt__(self, other):
+        pass
+
+    def __ge__(self, other):
+        pass
+
+    def __gt__(self, other):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __radd__(self, other):
+        pass
+
+    def __sub__(self, other):
+        pass
+
+    def __rsub__(self, other):
+        pass
+
+    def __or__(self, other):
+        pass
+
+    def __ror__(self, other):
+        pass
+
+    def __and__(self, other):
+        pass
+
+    def __rand__(self, other):
+        pass
+
+    def __iadd__(self, other):
+        pass
+
+    def __isub__(self, other):
+        pass
+
+    def __iand__(self, other):
+        pass
+
+    def __ior__(self, other):
+        pass
